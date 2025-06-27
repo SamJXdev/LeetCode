6c502a958f1a23d7c8283a72d000280d18514695
Jackson Samuvel <jackxsam07@gmail.com>
2025-06-27 15:48:53 +0530

    Time: 1 ms (74.48%), Space: 40.9 MB (60.05%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,30 +1,34 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (dividend == divisor)
+        if (dividend == divisor){
             return 1;
-
+        }
         boolean sign = true;
-        if ((dividend >= 0 && divisor < 0) || (dividend <= 0 && divisor > 0))
+        if(dividend>=0 && divisor<0){
             sign = false;
+        }
+        if(dividend<=0 && divisor>0){
+            sign = false;
+        }
 
-        long n = Math.abs((long) dividend);
-        long d = Math.abs((long) divisor);
-
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
         long quotient = 0;
 
-        while (n >= d) {
-            int cnt = 0;
-            while (n >= (d << (cnt + 1))) {
-                cnt++;
+        while(a>=b){
+            int count = 0;
+            while(a>=(b<<(count+1))){
+                count++;
             }
-            quotient += 1L << cnt;
-            n -= d << cnt;
+            quotient += 1L << count;
+            a-=b<<count;
         }
 
-        if (quotient >= (1L << 31)) {
-            return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        if(quotient>=(1L<<31)){
+            return sign ? Integer.MAX_VALUE : Integer.MIN_VALUE; 
         }
 
-        return sign ? (int) quotient : (int) -quotient;
+        return sign ? (int)quotient : (int)-quotient;
+        
     }
 }
