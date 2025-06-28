a6baa6717c002b689402cccceea2f04253d1ceef
Jackson Samuvel <jackxsam07@gmail.com>
2025-06-28 15:41:25 +0530

    Time: 2 ms (96.84%), Space: 41.9 MB (31.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,16 @@
 class Solution {
    public boolean isValid(String s) {
     Stack<Character> ramya = new Stack<>();
-                                                  // [   ( ,]   ]
-    for(char c: s.toCharArray()){
-        if(c=='(' || c=='{' || c=='['){
+                                                  
+    for(char c: s.toCharArray()){         
+        if(c=='(' || c=='{' || c=='['){  
             ramya.push(c);
         }
         else{
             if(ramya.isEmpty()){
                 return false;
             }
-            char top = ramya.pop();
+            char top = ramya.pop();   
             if( (c==')' && top!= '(') || (c=='}' && top!= '{' ) || ( c==']' && top!= '[') ){
                     return false;
                 }
