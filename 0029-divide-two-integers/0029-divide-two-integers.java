b757a644f72da64a5406c38ed1d9937e81828d3b
Jackson Samuvel <jackxsam07@gmail.com>
2025-06-30 21:11:22 +0530

    Time: 1 ms (74.31%), Space: 40.9 MB (75.71%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -4,24 +4,21 @@ class Solution {
         if((dividend>=0 && divisor<0)||(dividend<=0 && divisor>0)){
             sign = false;
         }
-
         long a = Math.abs((long)dividend);
         long b = Math.abs((long)divisor);
         long quotient = 0;
 
         while(a>=b){
-            int count = 0;
+            int count=0;
             while(a>=b<<(count+1)){
                 count++;
             }
             quotient += 1L << count;
-            a -= b <<count;
-        }
-
-        if(quotient>=1L<<31){
-            return sign ? Integer.MAX_VALUE: Integer.MIN_VALUE;
-        }
-
-        return sign ? (int)quotient: (int)-quotient;
+            a -= b<<count;       
+            }
+            if(quotient>=1L<<31){
+                return sign? Integer.MAX_VALUE:Integer.MIN_VALUE;
+            }
+            return sign? (int)quotient:(int)-quotient;
     }
 }
