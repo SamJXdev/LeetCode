d37bd79bea5dc32ac557dc1b80810fa1ba30e829
Jackson Samuvel <jackxsam07@gmail.com>
2025-06-30 21:34:35 +0530

    Time: 0 ms (100%), Space: 41 MB (7.69%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,16 +1,14 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        if(n==0){
-            return 1;
-        }
-        int ans =10;
+        if(n==0) return 1;
+        int unique_digits = 10;
         int start = 9;
-        int possibilities =9;
+        int current = 9;
 
         while(n-->1 && start>0){
-            possibilities *=start--;
-            ans += possibilities;
+            current *= start--;
+            unique_digits+=current;
         }
-        return ans;
+        return unique_digits;  
     }
 }
